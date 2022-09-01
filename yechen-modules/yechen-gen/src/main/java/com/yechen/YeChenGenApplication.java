21061e52e50e87df42b984706bdf10cb341dcf47
zhaoqi <945156573@qq.com>
2022-09-01 23:30:10 +0800

    基础结构

---
+++ yechen-modules/yechen-gen/src/main/java/com/yechen/YeChenGenApplication.java
@@ -0,0 +1,19 @@
+package com.yechen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 代码生成
+ *https://patorjk.com/software/taag/#p=display&f=Graffiti&t=YeChen%20Modules%20Gen
+ * @author zhao.qi
+ * @date 2022/9/1
+ * @apiNote
+ */
+@SpringBootApplication
+public class YeChenGenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YeChenGenApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  代码生成模块启动成功  ");
+    }
+}
